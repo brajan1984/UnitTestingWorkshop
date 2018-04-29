30f7cf1f6f74f1a61062edda25c113f16abe8b04
BRAJAN1984\bpiet <blazej.pietrzyk@gmail.com>
2018-04-29 18:10:21 +0200

    Step5: Resurection of primary "unit" test and check if it works with real service

--- UnitTestingWorkshopConsoleAppTests/UnitTest1.cs
+++ UnitTestingWorkshopConsoleAppTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using System;
 using FluentAssertions;
 using UnitTestingWorkshopConsoleApp;
+using UnitTestingWorkshopConsoleApp.Services;
+using UnitTestingWorkshopConsoleApp.Services.Interfaces;
 using Xunit;
 
 namespace UnitTestingWorkshopConsoleAppTests
@@ -8,8 +10,18 @@ namespace UnitTestingWorkshopConsoleAppTests
     public class UnitTest1
     {
         Solution _solutionImpl = new Solution(null, null);
+
+        public UnitTest1()
+        {
+            IHourProcessor _hourProcessor = new HourProcessor();
+            IUniqueNumberGenerator _numberGenerator = new TwoDigitsUniqueNumberGenerator();
+            IHourPartialsGenerator _hourPartialsGenerator = new HourPartialsGenerator(_numberGenerator);
+            IFullHoursGenerator _hoursGenerator = new FullHoursGenerator(_hourPartialsGenerator);
+
+            _solutionImpl = new Solution(_hourProcessor, _hoursGenerator);
+        }
         
-        /*[Theory]
+        [Theory]
         [InlineData(1, 8, 3, 2, 6, 4, "12:36:48")]
         [InlineData(4, 4, 4, 4, 4, 4, "NOT POSSIBLE")]
         [InlineData(1, 9, 3, 5, 6, 8, "16:38:59")]
@@ -21,6 +33,6 @@ namespace UnitTestingWorkshopConsoleAppTests
             var result = _solutionImpl.solution(A, B, C, D, E, F);
 
             result.Should().Be(expectedValue);
-        }*/
+        }
     }
 }
