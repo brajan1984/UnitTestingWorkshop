64ff4f82519a027e2ad7b5e52f6712119d0c5997
BRAJAN1984\bpiet <blazej.pietrzyk@gmail.com>
2018-04-29 16:43:00 +0200

    Step2-3: Writing unit tests for shells from Step2-2

--- UnitTestingWorkshopConsoleAppTests/Services/FullHoursGeneratorTests.cs
+++ UnitTestingWorkshopConsoleAppTests/Services/FullHoursGeneratorTests.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using Moq;
+using UnitTestingWorkshopConsoleApp.Models;
 using UnitTestingWorkshopConsoleApp.Services;
 using UnitTestingWorkshopConsoleApp.Services.Interfaces;
 using Xunit;
@@ -20,11 +23,26 @@ namespace UnitTestingWorkshopConsoleAppTests.Services
         public void GetAllPossibleHours_PassingSomeDigits_FlowIsCorrect()
         {
             //Arrange
+            var allDigits = new List<int>();
+            var hours = new List<Hour24Model>();
+            var hoursWMinutes = new List<Hour24Model>();
+            var fullHours = new List<Hour24Model>();
+
+            var myDigits = new List<int>();
+
+            _hourPartialsGenerator.Setup(h => h.FillAllHours(allDigits)).Returns(hours);
+            _hourPartialsGenerator.Setup(h => h.FillAllMinutes(allDigits, hours)).Returns(hoursWMinutes);
+            _hourPartialsGenerator.Setup(h => h.FillAllSeconds(allDigits, hoursWMinutes)).Returns(hoursWMinutes);
 
             //Act
+            var result = _hourGenerator.GetAllPossibleHours(allDigits);
 
             //Assert
-            Assert.False(true);
+            _hourPartialsGenerator.Verify(h => h.FillAllHours(It.Is<List<int>>(o => o == allDigits)), Times.Once);
+            _hourPartialsGenerator.Verify(h => h.FillAllMinutes(It.Is<List<int>>(o => o == allDigits), It.Is<List<Hour24Model>>(o => o == hours)), Times.Once);
+            _hourPartialsGenerator.Verify(h => h.FillAllSeconds(It.Is<List<int>>(o => o == allDigits), It.Is<List<Hour24Model>>(o => o == hoursWMinutes)), Times.Once);
+
+            result.Should().Equal(fullHours);
         }
     }
 }
